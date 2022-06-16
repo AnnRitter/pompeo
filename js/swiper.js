a990c3fcd05b05dfdfceefd81bb157e277f3bfa6
Anastasia Telnova <telnova.anastasia@gmail.com>
2022-06-16 23:30:32 +0300

    add swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,11 @@
 const swiper = new Swiper('.swiper', {
     speed: 400,
     spaceBetween: 100,
-  });
+    slidesPerView: 3,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  }, );
 
   
