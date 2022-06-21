2cf20980c48b5e1978b6208e5306988da1bff871
telnova.anastasia@gmail.com <telnova.anastasia@gmail.com>
2022-06-21 17:22:59 +0300

    changes 2106/2

--- js/swiper.js
+++ js/swiper.js
@@ -6,5 +6,31 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '" > ' + (index + 1) + ' </span>';
+      }
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 50,
+      },
+      738: {
+        slidesPerView: 2,
+        spaceBetween: 70,
+    },
+    1248: {
+        slidesPerView: 3,
+        spaceBetween: 100,
+      },
+      
+    }
+    
   }, );
 
